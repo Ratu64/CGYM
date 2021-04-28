791499dae826a822f91fe1b77b716c0c350d14ad
Ratu64 <62639105+Ratu64@users.noreply.github.com>
2021-04-28 18:00:47 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,15 +1,15 @@
 <?php
 session_start();
 require_once('config.php');
-//$qr='select Uid from survey where Uid = '.$_SESSION['id'].' ';
-//$query_run = mysqli_query($con,$qr);
-//if (mysqli_num_rows( $query_run) > 0) 
-// {
+$qr='select Uid from survey where Uid = '.$_SESSION['id'].' ';
+ $query_run = mysqli_query($con,$qr);
+ if (mysqli_num_rows( $query_run) > 0) 
+ {
 session_destroy();
 usleep(400000);
 header( "Location: index11.php");
-// }
-//else{
-//    header( "Location: survey.php");
-//}
+ }
+else{
+    header( "Location: survey.php");
+}
 ?>
