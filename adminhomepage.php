379e1708183937ef996d691d9dfe36ca0fbca798
Ratu64 <62639105+Ratu64@users.noreply.github.com>
2021-04-30 11:59:17 +0530

    Update adminhomepage.php

--- adminhomepage.php
+++ adminhomepage.php
@@ -28,7 +28,7 @@ if(isset($_POST["init"]))
 {
     date_default_timezone_set('Asia/Kolkata');
     $d = date("Y-m-d");
-    $query = "select * from recp_attendance where date=$d;";
+    $query = "select * from recp_attendance where date=".$d.";";
     $query_run = mysqli_query($con,$query);
     if($query_run)
     {
