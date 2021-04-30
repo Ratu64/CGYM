eb8c03cfbea5b831ad6e92f25da66a57674d7157
Ratu64 <62639105+Ratu64@users.noreply.github.com>
2021-04-30 12:01:32 +0530

    Update adminhomepage.php

--- adminhomepage.php
+++ adminhomepage.php
@@ -28,7 +28,7 @@ if(isset($_POST["init"]))
 {
     date_default_timezone_set('Asia/Kolkata');
     $d = date("Y-m-d");
-    $query = "select * from recp_attendance where date=".$d.";";
+    $query = "select * from recp_attendance where date="'.$d.'";";
     $query_run = mysqli_query($con,$query);
     if($query_run)
     {
@@ -37,7 +37,7 @@ if(isset($_POST["init"]))
             echo '<script type="text/javascript">alert("Attendance already initialized today")</script>';
         }
         else{
-                $ql= "INSERT into recp_attendance(email,date) select email,".$d." from users";
+                $ql= "INSERT into recp_attendance(email,date) select email,"'.$d.'" from users";
 		          $result = mysqli_query($con, $ql) ; 
             echo '<script type="text/javascript">alert("Attendance initialized!!")</script>';
         }
